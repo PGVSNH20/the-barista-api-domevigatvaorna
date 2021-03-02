316b52d805a1090c0531981140a97e710f1c87f1
fabbemannen <fabbemannen@gmail.com>
2021-03-02 15:52:46 +0100

    Basic stuff, patrik fixar resten

--- BaristaApi/Beverage.cs
+++ BaristaApi/Beverage.cs
@@ -1,21 +1,61 @@
+using System;
 using System.Collections.Generic;
 
 public interface IBeverage{
-	List<string> Ingredients { get; }
+	List<Ingredient> Ingredients { get; }
     string CupType { get; }
-    int Amount { get; }
+    IBeverage AddWater(int amount);
+    IBeverage AddMilk(int amount);
+    void ToBeverage();
+}
+
+public class Ingredient
+{
+    public string Name { get; set; }
+    public int Amount { get; set; }
 }
 
 class Espresso : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
-
+    public List<Ingredient> Ingredients { get; }
+ 
     public string CupType => throw new System.NotImplementedException();
+
+    public IBeverage AddWater(int amount)
+    {
+        Ingredients.Add(new Ingredient() { Name = "Water", Amount = amount});
+        return this;
+    }
+
+    public IBeverage AddMilk(int amount)
+    {
+        Ingredients.Add(new Ingredient() { Name = "Milk", Amount = amount });
+        return this;
+    }
+
+    public void ToBeverage()
+    {
+        foreach (var bp in Ingredients)
+        {
+            Console.WriteLine($"{bp.Name}: {bp.Amount} - ");
+        }
+    }
 }
 
-class Latte : IBeverage
-{
-    public List<string> Ingredients => throw new System.NotImplementedException();
+//class Latte : IBeverage
+//{
+//    public List<string> Ingredients => throw new System.NotImplementedException();
 
-    public string CupType => throw new System.NotImplementedException();
-}
+//    public string CupType => throw new System.NotImplementedException();
+
+//    public void AddWater(int amount) => throw new System.NotImplementedException();
+//    public void AddMilk(int amount)
+//    {
+//        throw new System.NotImplementedException();
+//    }
+
+//    public Espresso ToBeverage()
+//    {
+//        throw new System.NotImplementedException();
+//    }
+//}
--- BaristaApi/Program.cs
+++ BaristaApi/Program.cs
@@ -6,6 +6,9 @@ namespace BaristaApi
     {
         static void Main(string[] args)
         {
+            Espresso espresso = new Espresso();
+            espresso.AddWater(20).ToBeverage();
+
             /* pseudo-code
             Espresso espresso = new Espresso().AddWater(20).AddBeans(b => b.AmountInG = 5 && b.Sort = CoffeSorts.Robusta).ToBravage();
             //espresso is type of Espresso
